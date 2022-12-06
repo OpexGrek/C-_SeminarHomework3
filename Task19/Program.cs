07dba0841ca94f4663299ee0b1b60cac91078e04
Oleg <starwarser2@gmail.com>
2022-12-06 11:56:29 +0700

    Task19 ввод дополнительных проверок на ошибочный ввод

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,17 +4,17 @@ Console.Clear();
 Console.WriteLine("Input 5 numbers");
 string number = Convert.ToString(Console.ReadLine());
 
-// Проверка на ввод букв
-bool isNumber=double.TryParse(number, out double numericCheck);
+// Проверка на ввод букв и отрицательных чисел
+bool isNumber = double.TryParse(number, out double numericCheck);
 
-if (numericCheck==0)
-    Console.WriteLine("You should input only numbers");
+if (numericCheck <= 0)
+    Console.WriteLine("You should input only 5 numbers");
 else
 // Проверка на ввод более или менее 5 цифр    
     if (number.Length > 5 || number.Length < 5)
         Console.WriteLine("You should input only 5 numbers");
     else
-    // Выявление палиндрома
+        // Выявление палиндрома
         if (number[0] == number[4] && number[1] == number[3])
             Console.WriteLine("Number " + number + " is palindrom");
         else
